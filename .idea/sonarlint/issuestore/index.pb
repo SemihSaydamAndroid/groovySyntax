77272e6a719993e4b8dc7fc35a6e47f55724dc97
SemihSaydamAndroid <semihsaydamz@gmail.com>
2021-07-13 05:58:00 +0300

    initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+K
+src/helloworldScript.groovy,a/c/ac69e5e2ea8e6c93a7d3f304dad0fb4a8c7d3c08
---
+++ src/HelloWorld.groovy
@@ -0,0 +1,23 @@
+class HelloWorld {
+    static void main(String[] args) {
+
+        def name = "Semih"
+        name = 40
+
+        println("10 + 2 = " + (10 + 2))
+        println("10 - 2 = " + (10 - 2))
+        println("10 * 2 = " + (10 * 2))
+        println("10 / 2 = " + (10.intdiv(2)))
+        println("10 % 2 = " + (10 % 2))
+        println("\n")
+
+        //Float
+        println("6.2 + 4.5 = " + (6.2.plus(4.5)))
+        println("6.2 - 4.5 = " + (6.2.minus(4.5)))
+        println("6.2 * 4.5 = " + (6.2.multiply(4.5)))
+        println("6.2 / 4.5 = " + (6.2 / 4.5))
+        
+        
+
+    }
+}
