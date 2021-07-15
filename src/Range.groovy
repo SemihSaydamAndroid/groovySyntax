7bec1250cee52f1263954401d5f6498c78a70645
SemihSaydamAndroid <semihsaydamz@gmail.com>
2021-07-15 22:54:27 +0300

    Range added

---
+++ src/Range.groovy
@@ -0,0 +1,17 @@
+/**
+ * @author Semih Saydam
+ * @version 0.0.1
+ */
+class Range {
+
+    static void main(String[] args) {
+        def oneToFive = 1..5
+        def aToZ = 'a'..'z'
+        def zToA = 'z'..'a'
+
+        println("Size integer range : " + oneToFive.size())
+        println("2nd item is : " + oneToFive.get(1))
+        println("Size alphabet range : " + aToZ.size())
+    }
+
+}
